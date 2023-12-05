6f68be67a24c5e5868b5a50c915a8847c2299085
MiguelLaIglesia <146960524+MiguelLaIglesia@users.noreply.github.com>
2023-12-05 12:33:48 +0100

    Add files via upload

--- Assignment_3/main.rb
+++ Assignment_3/main.rb
@@ -17,13 +17,7 @@ else
     abort "Incorrect order of files passed. Usage: ruby main.rb ArabidopsisSubNetwork_GeneList.txt"
 end
 
-
-
-
-
 #------------------------ PUBLIC INSTANCE METHODS -----------------------------------------------------------------------
-
-
 
 # Get response from URL
 def fetch(uri_str) 
@@ -71,7 +65,7 @@ def read_from_file(filename)
     return gene_ids
 end
 
-
+# Gets coordinates from string
 def get_coordinates(coordinates_string)
     position = coordinates_string.match(/\d+\.\.\d+/)[0]
 
@@ -79,185 +73,165 @@ def get_coordinates(coordinates_string)
     end_coordinate = position.split('..')[1].to_i
     return start_coordinate, end_coordinate
 end
+
+
+def find_motifs (sequence, motif)
+    matches = []
+    position = 0
+  
+    while (match = sequence.match(motif, position))
+        match_sequence = match[0]
+        start_motif = match.begin(0)
+        end_motif = match.end(0)
+  
+        matches << {
+          sequence: match_sequence,
+          start: start_motif.to_i,
+          end: end_motif.to_i
+        }
+  
+        # Move the position forward
+        position = end_motif
+    end
+  
+    return matches
+end
 #-------------------------------------------MAIN CODE ------------------------------------------------------------------
+                
+                # DESACTIVADO TODO, ACTIVAR PARA ENTREGA
+                # 1:  Using BioRuby, examine the sequences of the ~167 Arabidopsis genes from the last assignment by retrieving them from whatever database you wish #
+                # ArabidopsisSubNetwork file -> each gene locus (lines) -> search for embl file -> add to 'AT_sequences.embl'
 
-# 1:  Using BioRuby, examine the sequences of the ~167 Arabidopsis genes from the last assignment by retrieving them from whatever database you wish
-# ArabidopsisSubNetwork file -> each gene locus (lines) -> search for embl file -> add to 'AT_sequences.embl'
-# DESACTIVADO TODO, ACTIVAR PARA ENTREGA
+                #puts "Processing #{gene_file} file, this might take a while..."
 
-#puts "Processing #{gene_file} file, this might take a while..."
+                # Create string with gene IDs from file
+                #gene_ids = read_from_file(gene_file)
 
-# Create string with gene IDs from file
-#gene_ids = read_from_file(gene_file)
+                # Only one search with list of IDs
+                #response_body = ncbi_fetch(database = 'ensemblgenomesgene',file_format = 'embl', id = gene_ids)
 
-# Only one search with list of IDs
-#response_body = ncbi_fetch(database = 'ensemblgenomesgene',file_format = 'embl', id = gene_ids)
+                #output_file = File.open('AT_sequences.embl', 'w')
+                #output_file.write(response_body)
+                #output_file.close
 
-#output_file = File.open('AT_sequences.embl', 'w')
-#output_file.write(response_body)
-#output_file.close
+                # Comprobado que da los 168
 
-# Comprobado que da los 168
-
-# 2: Loop over every exon feature, and scan it for the CTTCTT sequence
+# 2: Loop over every exon feature, and scan it for the CTTCTT sequence #
 
 embl_file = Bio::FlatFile.auto('AT_sequences.embl')
-#puts embl_file.class  # Bio::FlatFile
-
-#entry = embl_file.next_entry
 
 search_positive = Bio::Sequence::NA.new("cttctt")
 search_complementary = Bio::Sequence::NA.new("aagaag")
+
 regex_positive = Regexp.new(search_positive.to_re)
 regex_complementary = Regexp.new(search_complementary.to_re)
+
 i = 0
 
+start_gene = nil
+end_gene = nil
+
+# Loop each entry in EMBL file
 embl_file.each_entry do |entry|
 
     break if i > 2
     i += 1
 
-    next unless entry.accession 
+    next unless entry.accession # Lack of accesion is suspicious
 
     puts
-    puts "NUEVA ENTRY #{entry.definition} #{entry.class}"
-    # Extract sequence coordinates from entry.definition
-    entry_definition = entry.definition
-    match = entry_definition.match(/\d+\.\.\d+/)
-    if match
-        coordinates = match[0]
-        start_entry = coordinates.split('..')[0].to_i
-        end_entry = coordinates.split('..')[1].to_i
-    end
+    puts "NEW ENTRY #{entry.definition}"
+    
+    # Extract sequence chromosome coordinates from entry.definition
+    start_entry, end_entry = get_coordinates(entry.definition)
 
+    # Anotate sequence chromosome coordinates
     entry_sequence = entry.to_biosequence
-    f1 = Bio::Feature.new('seq_coordinates',coordinates) # feature type and position
+    f1 = Bio::Feature.new('chromosomal_coordinates',entry.definition) # 'feature type' , 'position'
     f1.append(Bio::Feature::Qualifier.new('start', start_entry))
     f1.append(Bio::Feature::Qualifier.new('end', end_entry))
     entry_sequence.features << f1
 
-    start_entry_internal = 1
-    end_entry_internal = end_entry - start_entry
+    entry_id = entry_sequence.entry_id
+    puts "AAAAA #{entry_id}"
 
-    #puts entry_sequence.length
-    #entry_sequence = entry.naseq
-    #entry_sequence = entry.sequence
-
-    #puts entry_sequence.class
-    #entry_id = entry_sequence.entry_id
-    #entry_end = entry_sequence.end
-
-    #puts "#{entry_id}"
-    #entry_position = entry.position
-    #puts  entry_position
-    #puts sequence
-    #puts sequence.seq
-    #seq = Bio::Sequence::NA.new("atgcatgcaaaa")
+    # LOOP FEATURES
     entry.features.each do |feature|
         featuretype = feature.feature
+        position = feature.position 
+        qual = feature.assoc
+
+        if featuretype == 'source'
+            source = qual['db_xref']
+        end
+
+        if featuretype == 'CDS' && !qual['codon_start'].empty?
+            phase = qual['codon_start']
+        else
+            phase = '.'
+        end
 
         next unless featuretype == 'exon'
 
-        position = feature.position 
-        puts
-        puts "Longitud secuencia #{entry_sequence.length}, Coordenadas internas: #{start_entry_internal}..#{end_entry_internal}"
-        puts "Position exon: #{position}"
-        #next
+        # Get exon ID
+        qual = feature.assoc
+        exon_id = qual["note"]
 
-        # Internal coordinates: to search in the sequence
-        # Universal coordinates: to annotate as new feature
-        # Gene coordinates
+        # Get exon coordinates
+        start_exon, end_exon = get_coordinates(position)
+        
+        # Get exon sequence from its coordinates, and test if that exon is inside the entry sequence to continue or not
+        exon_sequence = entry_sequence.subseq(start_exon, end_exon)
+        next if exon_sequence.nil? || exon_sequence.empty?
 
+        # Decides wether to use positive or complement regular expression
         if position.include?('complement')
-            # Las coordenadas de la CADENA NEGATIVA son universal coordinates
-            position = position.match(/\d+\.\.\d+/)[0] # quitas ese 'complement y parentesis'
-            
-            start_exon = position.split('..')[0].to_i
-            end_exon = position.split('..')[1].to_i
-
-            start_exon_internal = start_exon - start_entry 
-            end_exon_internal = end_exon - start_exon + start_exon_internal
-
+            regex = regex_complementary
+            strand = '-'
         else
-            # Las coordenadas de CADENA POSITIVA son internal coordinates
-            start_exon_internal = position.split('..')[0].to_i
-            end_exon_internal = position.split('..')[1].to_i
-
-            start_exon = start_exon_internal + start_entry - 1
-            end_exon = end_exon_internal + start_entry
+            regex = regex_positive
+            strand = '+'
         end
+
+        # Find motifs in exon sequence and retrieves the positions (BUT IN EXON SEQUENCE). It works for several matches/motifs in the exon.
+        matches_motifs = find_motifs(exon_sequence, regex)
         
-        # AAAG CTTCTT GGTA
-
-        # TTTC GAAGAA CCAT
+        next if matches_motifs.empty? || matches_motifs.nil?
         
-    
-        puts "Coordenadas secuencia: #{start_entry} #{end_entry}"
-        puts "Coordenadas exon universales:#{start_exon} #{end_exon}"
-        puts "Coordenadas exon internas: #{start_exon_internal} #{end_exon_internal}"
+        # For each matched motif in exon sequence
+        matches_motifs.each do |match|
 
-        next
-        #exon_sequence = entry_sequence.subseq(start_subsequence,end_subsequence)
+            # First and last position of motif, BUT IN EXON SEQUENCE
+            start_match = match[:start]
+            end_match = match[:end]
 
-        #puts exon_sequence
+            # Conversion from exon positions to entry sequence positions
+            start_motif = start_exon + start_match # +1 por ccs del match, -1 por la suma de exon y match
+            end_motif = start_exon + end_match - 1 # -1 por la suma de exon y match
 
+            #puts "Motif #{entry_sequence.subseq(start_motif, end_motif)} found at position #{start_motif} to #{end_motif}"
 
-        # DIFERENCIAR ENTRE COMPLEMENT O NO PARA HCER SPLIT DISTINTO Y TRABAJAR CON COORDENADAS INTERNAS O TOTALES
-        # CREAR OBJETO SECUENCIA DEL EXON CON START Y END, YO DIRIA YA REFERIDA A TOTALES (ENTONCES LA + TRANSFORMAR A TOTALES, EASY)
-        # Y DE AHI BUSCAR EN LA SECUENCIA SI HAY CTTCTT CON EL MATCH
+            # Create new feature for entry sequence with the information of the motif found in exon
+            #if regex == regex_complementary
+                #feature_position = "complement(#{start_motif}..#{end_motif})"
+            #    strand = '-'
+            #else
+                #feature_position = "#{start_motif}..#{end_motif}"
+            #    strand = '+'
+            #end
 
-        #start_exon = position.split('..')[0]
-        #end_exon = position.split('..')[1]
+            f1 = Bio::Feature.new('repeat_region', source) # 'feature type' , 'position'
+            f1.append(Bio::Feature::Qualifier.new('seqid', entry_id))
+            f1.append(Bio::Feature::Qualifier.new('start', start_motif))
+            f1.append(Bio::Feature::Qualifier.new('end', end_motif))
+            f1.append(Bio::Feature::Qualifier.new('strand', strand))
+            f1.append(Bio::Feature::Qualifier.new('score', '.')) # Score is normaly given inside "note" in CDS, in this case there is no score for any of them
+            entry_sequence.features << f1                        # Phase is normaly shown as "codon_start" in CDS
+            
+        end
 
-        #puts start_exon
-        #puts end_exon
-        
-
-        #puts featuretype
-        #if featuretype == 'seq_coordinates' # estan conectados entry y entry_sequence
-        #    
-        #    puts "AAAAAAAAAAA #{feature.position}"
-        #end
-        
-        # OBTENER COORDENADAS INTERNAS DE LA SECUENCIA
-        #if feature.feature == "source"
-        #    puts feature 
-        #    puts feature.position
-        #end
-
-        #qual = feature.assoc
-        #puts "#{feature} #{feature.position} #{qual}"
-        
-        #exon_seq = seq.subseq(3,8)
-
-        #match = exon_seq.seq.match(regex)
-
-        #qual = feature.assoc            # feature.assoc gives you a hash of Bio::Feature::Qualifier objects 
-        #puts qual
-        #puts position
-        #puts "NUEVA FEATURE #{qual}"                                # i.e. qualifier['key'] = value  for example qualifier['gene'] = "CYP450")
-        #next unless qual['exon']    # this is an indication that the feature is a transcript
-
-        #exon = qual["note"]
-        #puts "#{exon}"
-        #puts exon
-        # collects gene name and exon positions and joins it into a string
-        #gene_info = [qual['gene'], qual['exon']].compact.join(', ')
-        #puts "aaaaa #{exon}"
     end
 end
+        
 
 
-
-
-
-
-
-
-
-# LOOP FILE ENTRIES (MÁS EN JUPYTER)
-#embl_file.each_entry do |entry|
-#    next unless entry.accession 
-#    puts entry.class
-#    puts "The record is #{entry.definition} "
-#  end
