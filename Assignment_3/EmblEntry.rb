2c0af5aa0bd92d5cd464dc7cc4c06010c920c830
MiguelLaIglesia <146960524+MiguelLaIglesia@users.noreply.github.com>
2023-12-06 21:25:17 +0100

    Add files via upload

--- Assignment_3/EmblEntry.rb
+++ Assignment_3/EmblEntry.rb
@@ -1,28 +1,75 @@
+
+# == EmblEntry
+#
+# Object representing each entry of the .embl file (gene's sequence informatiion)
+#
+# == Summary
+#
+# Class used to represent information about the sequences of a given gene or list of genes, employing objects and methods of BioRuby gem
+#
+
 class EmblEntry
 
-    attr_accessor :entry_sequence, :chromosomal_coordinates, :source, :accession, :seq_id, :gene_locus
+    # Get/Set method for the genomic sequence of a given gene
+    # @!attribute [rw] entry_sequence
+    # return [Bio::Sequence] the genomic sequence 
+    attr_accessor :entry_sequence
 
+    # Get/Set method for the chromosomal coordinates of a given gene
+    # @!attribute [rw] chromosomal_coordinates
+    # return [Array<Integer>] the chromosomal coordinates as a two-elemnt array
+    attr_accessor :chromosomal_coordinates
+
+    # Get/Set method for the source of the annotation for a given gene
+    # @!attribute [rw] source
+    # return [String] source of annotation
+    attr_accessor :source
+
+    # Get/Set method for the accession ID of a gene/entry
+    # @!attribute [rw] accession
+    # return [String] accession ID
+    attr_accessor :accession
+
+    # Get/Set method for the sequence ID of a gene/entry
+    # @!attribute [rw] seq_id
+    # return [Array<Integer>] the sequence ID
+    attr_accessor :seq_id
+
+    # Get/Set method for the gene locus name of a given gene
+    # @!attribute [rw] gene_locus
+    # return [Array<Integer>] the gene locus name
+    attr_accessor :gene_locus
+
+    # It creates a new instance of EmblEntry
+    #
+    # @param entry[Bio::EMBL] the BioRuby EMBL entry of a given gene
+    # @param start_entry [Integer] the start chromosomal coordinate of the gene
+    # @param end_entry [Integer] the en chromosomal coordinate of the gene
+    # return [EmblEntry] Just created EMBL entry
     def initialize(entry, start_entry, end_entry)
-        @accession = entry.accession
+        @accession = entry.accession  # get the ID line from that entry
         @seq_id = entry.entry_id
-        @entry_sequence = entry.to_biosequence
-        @chromosomal_coordinates = [start_entry, end_entry]
+        @entry_sequence = entry.to_biosequence  # get biosequence of nucleotides
+        @chromosomal_coordinates = [start_entry, end_entry] # chromosomic coordinates of that entry
         @source = ""
         @gene_locus =""
     end
 
+    # It annotates the gene locus of the gene as a feature of the Bio::Sequence (genomic sequence) object
+    # @!method no_params_method
+    # return [void]
     def annotate_source_gene
         @entry_sequence.features.each do |feature|
           featuretype = feature.feature
           position = feature.position
           qual = feature.assoc
           
-          # Annotate source
+          # Annotate data base taxon
           if featuretype == 'source'
             @source = qual['db_xref']
           end
           
-          # Annotate gene locus for the sequence
+          # Annotate gene name for the sequence
           if featuretype == 'gene' && position.match(/^(complement\()?(\d+\.\.\d+)(\))?$/) && @gene_locus.empty?
             @gene_locus = qual['gene']
           end
@@ -32,38 +79,58 @@ class EmblEntry
     end
 
 
-    # Add found repeats as feature of the Embl biosequence object
-    def add_cttctt_repeat(coordinates, start_motif, end_motif, strand, source)
-        return if @entry_sequence.features.any? { |f| f.feature == 'cttctt_repeat' && f.position == coordinates }   # check if this motif has already been annotated
-    
-        f1 = Bio::Feature.new('cttctt_repeat', coordinates) # New featyre of the Embl biosequence object --> repetitive motifs "CTTCTT"
+    # Adds a found CTTCTT motif as feature of the EMBL Bio::Sequence object
+    #
+    # @param coordinates [String] the start..end coordinates of the motif (in the gene's sequence or chromosome)
+    # @param start_motif [Integer] the start coordinate of the motif (gene's sequence or chromosomal)
+    # @param end_motif [Integer] the end coordinate of the motif (gene's sequence or chromosomal)
+    # @param strand [String] genomic strand were motif can be found (+ or -)
+    # return [void]
+    def add_cttctt_repeat(coordinates, start_motif, end_motif, strand)
+
+        # Check if that cttctt region has already been annotated
+        return if @entry_sequence.features.any? { |f| f.feature == 'cttctt_repeat' && f.position == coordinates }
+
+        # Creates new feature for repetitive motifs keeping coordinates
+        f1 = Bio::Feature.new('cttctt_repeat', coordinates)
         f1.append(Bio::Feature::Qualifier.new('start', start_motif))
         f1.append(Bio::Feature::Qualifier.new('end', end_motif))
         f1.append(Bio::Feature::Qualifier.new('strand', strand))
         f1.append(Bio::Feature::Qualifier.new('SO_Name', 'repeat_region'))
-        @entry_sequence.features << f1
+        @entry_sequence.features << f1  # record as feature
     end
 
-    # convert motif sequnce-coordenates into chromosomal-coordenates for second part of the task
+    # Converts a motif gene's sequence coordenates to chromosomal coordenates
+    #
+    # @param start_motif [Integer] the start coordinate of the motif (gene's sequence or chromosomal)
+    # @param end_motif [Integer] the end coordinate of the motif (gene's sequence or chromosomal)
+    # return [Array<Integer>] the chromosomal coordinates of the CTTCTT motif
     def convert_to_chromosomal_coordinates(start_motif, end_motif)
-        start_chr = @chromosomal_coordinates[0]
-        length_motif = end_motif - start_motif + 1
-        start_motif = start_motif + start_chr - 1
-        end_motif = start_motif + length_motif - 1
+        start_chr = @chromosomal_coordinates[0] # chromosomal start position
+        length_motif = end_motif - start_motif
+        start_motif = start_motif + start_chr - 1 # -1 because of the sum of coodenates
+        end_motif = start_motif + length_motif
         return [start_motif, end_motif]
     end
 
+    # Finds CTTCTT repeats in exons in either the positive strand or the complement. It retrieves coordinates dependent of the reference system (chromosome or sequence)
+    #
+    # @param coordinates_to_use [String] reference system to calculate motif's coordinates
+    # @param regex_positive [Regexp] regular expression for searching for the CTTCTT pattern in the positive strand
+    # @param regex_complementary [Regeexp] regular expresion for searching for the CTTCTT pattern in the complementary strand 
+    # return [void]
     def process_cttctt_repeats(coordinates_to_use, regex_positive=REGEX_POSITIVE, regex_complementary = REGEX_COMPLEMENT)
 
         @entry_sequence.features.each do |feature|
-          next unless feature.feature == 'exon' && feature.position.match?(/^(complement\()?(\d+\.\.\d+)(\))?$/)    # check for exons
+          next unless feature.feature == 'exon' && feature.position.match?(/^(complement\()?(\d+\.\.\d+)(\))?$/) # only exon features
     
           start_exon, end_exon = get_coordinates(feature.position)
-          exon_sequence = @entry_sequence.subseq(start_exon, end_exon)  # get exon subsequence from exon coordinates
+          exon_sequence = @entry_sequence.subseq(start_exon, end_exon)  # get nucleotide sequence for exon
     
-          next if exon_sequence.nil? || exon_sequence.empty?    # control: is empty?
-    
-          if feature.position.include?('complement')    # get directionality of exon (positive or complement strand)
+          next if exon_sequence.nil? || exon_sequence.empty?
+          
+          # Tests if exon in + or - strand
+          if feature.position.include?('complement')
             regex = regex_complementary
             strand = '-'
             coordinates_format = "complement(%s)"
@@ -73,26 +140,25 @@ class EmblEntry
             coordinates_format = "%s"
           end
         
-          # Find motifs in exon sequence and retrieves the positions (BUT IN EXON SEQUENCE). It works for several matches/motifs in the exon.
+          # Finds coordinates in the repetitive region in exon, if existing
           matches_motifs = find_motifs(exon_sequence, regex)
           next if matches_motifs.empty? || matches_motifs.nil?
         
           # For each matched motif in exon sequence
           matches_motifs.each do |match|
-            # First and last position of motif, BUT IN EXON SEQUENCE
             start_match = match[:start]
             end_match = match[:end]
             # Conversion from exon positions to entry sequence positions
-            start_motif = start_exon + start_match  # +1 por ccs del match, -1 por la suma de exon y match
-            end_motif = start_exon + end_match - 1  # -1 por la suma de exon y match
+            start_motif = start_exon + start_match  # +1 for object index in ruby, -1 because of the sum of coordenates
+            end_motif = start_exon + end_match - 1  # -1 because of the sum of coodenates
     
-             # If chromosomal coordinates, it recalculates those motif coordinates in the chromosomal coordinates
+             # Recalculates for chromosomal coordinates as reference if specified
             if coordinates_to_use == 'chromosomal coordinates'
                 start_motif, end_motif = convert_to_chromosomal_coordinates(start_motif, end_motif)
             end
 
             coordinates = coordinates_format % "#{start_motif}..#{end_motif}"
-            add_cttctt_repeat(coordinates, start_motif, end_motif, strand, @source)
+            add_cttctt_repeat(coordinates, start_motif, end_motif, strand) # Creates new feature in Bio Ruby object
             
           end
         end
--- Assignment_3/main.rb
+++ Assignment_3/main.rb
@@ -4,17 +4,16 @@ require 'net/http'
 require  './EmblEntry.rb'
 require './EmblProcessor.rb'
 
-#------------------------------ INPUT FILES NAMES AS ARGUMENTS FROM  COMMAND LINE ---------------------------------------
+#------------------------------ INPUT FILES NAMES AS ARGUMENTS FROM COMMAND LINE ----------------------------------------
 
 # Check for one common error: not specifying the correct number of files needed for the program to run
 if ARGV.length != 1
-    abort "Incorrect number of files passed. Two files names must be specified: input list of genes and name for final_report"
+    abort "Incorrect number of files passed. Gene file name must be specified: input list of genes"
 end
   
 # Check for second common error: incorrect usage, files in incorrect order or wrong name passed.
 if ARGV[0] == "ArabidopsisSubNetwork_GeneList.txt"
-#if ARGV[0] == 'soloungen.txt'
-        gene_file = ARGV[0]
+    gene_file = ARGV[0]
 else 
     abort "Incorrect order of files passed. Usage: ruby main.rb ArabidopsisSubNetwork_GeneList.txt"
 end
@@ -22,7 +21,9 @@ end
 
 # ------------ PUBLIC INSTANCE METHODS ---------------------------------------
 
-# Get response from URL
+# Gets a response from a given URL
+#
+# @param uri_str [String] URL to make the get request to 
 def fetch(uri_str) 
     address = URI(uri_str)  
     response = Net::HTTP.get_response(address)
@@ -36,7 +37,11 @@ def fetch(uri_str)
     end
 end
 
-# Get response with NCBI E-Utils, given a database, file format and gene locus
+# Programmatic access to NCBI E-Utils,
+#
+# @param database [String] the database to retrieve gene's information
+# @param file_format [String] type of format for the response
+# @param gene_locus [String] the gene's locus name to retrieve info about.
 def ncbi_fetch(database, file_format, id)
     url = "http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=#{database}&format=#{file_format}&id=#{id}"
     #puts url
@@ -45,52 +50,70 @@ def ncbi_fetch(database, file_format, id)
 end
 
 
-# Transform locus names file into string of coma separated locus names, while checking A. thaliana format
+# Read all genes (locus names in the file) and create a comma-separated string of them. Check for correct gene format and species
+#
+# @param filename [String] Name of the file with the locus names of the genes to retrieve sequence's info about
+# return gene_ids [Array<String>] array with all gene ids
 def read_from_file(filename)
     gene_list = []
     gene_file = File.open(filename, 'r')
     gene_file.readlines.each do |line|  
         locus_name=line.chomp
-        if locus_name !~ /A[Tt]\d[Gg]\d\d\d\d\d/ # Check for correct format of gene locus name
+        if locus_name !~ /A[Tt]\d[Gg]\d\d\d\d\d/ # Checking proper arabidopsis thaliana gene locus
             abort "Locus name #{locus_name} does not meet the correct format. Please define locus names as AT0g00000"
         end
         gene_list << locus_name 
     end
     gene_file.close
-    gene_ids = gene_list.join(",")
+    gene_ids = gene_list.join(",")  # All gene IDs will be retrieved at once from embl
     return gene_ids
 end
 
-# Gets coordinates from string
+# Given a string with all cordinates it splits and finds start and end coordinates
+#
+# @param coordinates_string [String] string of coordinates to retrieve start and end coordinates
+# return [Array<Integer>] retrieved coordinates as a two-element array
 def get_coordinates(coordinates_string)
-    position = coordinates_string.match(/\d+\.\.\d+/)[0]
+    position = coordinates_string.match(/\d+\.\.\d+/)[0]  # To match coordinates
     start_coordinate = position.split('..')[0].to_i
     end_coordinate = position.split('..')[1].to_i
     return[start_coordinate, end_coordinate]
 end
 
-# Given an specific motif (sequence pattern) get start and end coordinates of any match in a given sequence
+# Given an specific motif (sequence pattern) get its start and end coordinates of any match in a given sequence
+#
+# @param sequence [String] sequence string to find the specific pattern
+# @param motif [String] string pattern 
+# return matches [Array<String>] array with all matches (found motifs) and its start and end coordinates (referenced to the sequence string param)
 def find_motifs (sequence, motif)
     matches = []
-    position = 0
+    position = 0    # Start position to search
     while (match = sequence.match(motif, position))
-        match_sequence = match[0]
-        start_motif = match.begin(0)
-        end_motif = match.end(0)
-        matches << {
+        match_sequence = match[0]  
+        start_motif = match.begin(0)    # Position of the first character to match
+        end_motif = match.end(0)        # Position of the second character to match
+        matches << {   
           sequence: match_sequence,
           start: start_motif.to_i,
           end: end_motif.to_i
         }
-        # Move the position forward, "- 5" allows to detect a domain "CTTCTTCTT", and that will be detected as 2
-        position = end_motif - 5
+
+        position = end_motif - 5    # Move the searching position for next match, if existing
     end
     return matches
 end
 
 # Write a GFF3 file
+#
+# @param gff [Bio::GFF::GFF3] Bio::GFF object to be printed out into a file
+# @param coordinates_to_use [String] Reference system to calculate motif's coordinates respect with 
+
 def write_gff(gff, coordinates_to_use)
-    file = coordinates_to_use == 'chromosomal coordinates' ? 'AT_repeats_chromosomal.gff' : 'AT_repeats_sequence.gff'
+    if coordinates_to_use == 'chromosomal coordinates'
+        file = 'AT_repeats_chromosomal.gff'
+    else
+        file = 'AT_repeats_sequence.gff'
+    end
     File.open(file, 'w') do |file|
       file.puts gff.to_s
     end
@@ -98,39 +121,36 @@ end
   
 #-------------------------------------------MAIN CODE ------------------------------------------------------------------
 
-# 1:  Using BioRuby, examine the sequences of the ~167 Arabidopsis genes from the last assignment by retrieving them from whatever database you wish #
-# ArabidopsisSubNetwork file -> each gene locus (lines) -> search for embl file -> add to 'AT_sequences.embl'
+# Task 1
 
 puts "Processing #{gene_file} file, this might take a while..."
 
-# Create string with gene IDs from file
-gene_ids = read_from_file(gene_file)
+gene_ids = read_from_file(gene_file) # Array of gene ids
+response_body = ncbi_fetch(database = 'ensemblgenomesgene',file_format = 'embl', id = gene_ids) # One NCBI search for all genes 
 
-# Only one search with list of IDs
-response_body = ncbi_fetch(database = 'ensemblgenomesgene',file_format = 'embl', id = gene_ids)
+filename = 'AT_sequences.embl' # Name of the embl file
 
-filename = 'AT_sequences.embl'
 output_file = File.open(filename, 'w')
 output_file.write(response_body)
 output_file.close
 
 
-search_positive = Bio::Sequence::NA.new("cttctt")
-search_complementary = Bio::Sequence::NA.new("aagaag")
+search_positive = Bio::Sequence::NA.new("cttctt")   # Motif to look in positive strand
+search_complementary = Bio::Sequence::NA.new("aagaag")  # Motif to look for in negative strand
 REGEX_POSITIVE = Regexp.new(search_positive.to_re)
 REGEX_COMPLEMENT = Regexp.new(search_complementary.to_re)
 
 
-# 2, 3, 4a and 4b
+# Tasks 2, 3, 4a and 4b
 
 eobject_processor = EmblProcessor.new(filename)
-gff_seq = eobject_processor.load_to_gff('sequences coordinates')
+gff_seq = eobject_processor.load_to_gff('sequences coordinates')    # Create the GFF3 file referred to the gene's sequence coordinates
 write_gff(gff_seq, 'sequences coordinates')
 
-# 5 and 6
+# Tasks 5 and 6
 
 eobject_processor_chr = EmblProcessor.new(filename)
-gff_chr = eobject_processor_chr.load_to_gff('chromosomal coordinates')
+gff_chr = eobject_processor_chr.load_to_gff('chromosomal coordinates')  # Create the GFF3 file referred to whole chromosome
 write_gff(gff_chr,'chromosomal coordinates')
 
 
